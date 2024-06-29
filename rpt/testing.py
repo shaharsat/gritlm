57c434c504f73ef631c0f0e916bbce9cf998b5a2
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-06-29 19:11:13 +0300

    bfloating all the way

--- rpt/testing.py
+++ rpt/testing.py
@@ -1,5 +1,5 @@
+"""
 import sys
-
 import torch
 
 sys.path.append('/tmp/shahar/gritlm/')
@@ -8,5 +8,13 @@ from rpt.neox_model_torch import GPTNeoXForCausalLM
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1').to(device)
+encode_output = hf_model.encode(['hello'*100, 'world'], 1)
+print(encode_output)
+"""
+import torch
+
+from rpt.neox_model_torch import GPTNeoXForCausalLM
+
+hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1-bf16', torch_dtype=torch.bfloat16)
 encode_output = hf_model.encode(['hello'*100, 'world'], 1)
 print(encode_output)
