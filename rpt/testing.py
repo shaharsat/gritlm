529b857ff0d89e2ebdffccf73300c9feeb679442
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-06-29 18:00:55 +0300

    .

--- rpt/testing.py
+++ rpt/testing.py
@@ -1,6 +1,10 @@
+import sys
+
+sys.path.append('/tmp/shahar/gritlm/')
+
+
 from rpt.neox_model_torch import GPTNeoXForCausalLM
 
 hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1')
-hf_model.config.input_length = 2048
 encode_output = hf_model.encode(['hello'*100, 'world'], 1)
 print(encode_output)
