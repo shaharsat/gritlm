b6b061a28b2c671e74d084c535a92f389b632993
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-06-29 19:11:36 +0300

    .

--- rpt/testing.py
+++ rpt/testing.py
@@ -7,7 +7,7 @@ from rpt.neox_model_torch import GPTNeoXForCausalLM
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1').to(device)
+hf_model = GPTNeoXForCausalLM.from_pretrained('Shahar603/neox-rpt-1', torch_dtype=torch.bfloat16).to(device)
 encode_output = hf_model.encode(['hello'*100, 'world'], 1)
 print(encode_output)
 """
