9bdf4be14a3a55c3bca1c7710e9d6321f23c1bc0
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-07-08 00:16:19 +0300

    .

---
+++ rpt/mteb_test.py
@@ -0,0 +1,20 @@
+import torch
+import sys
+sys.path.append('/a/home/cc/students/cs/ohadr/netapp/shahar_s/gritlm')
+sys.path.append('/Users/shahar.satamkar/Desktop/research/gritlm')
+print(sys.path)
+from mteb import AmazonReviewsClassification, Banking77Classification
+from rpt import GPTNeoXForCausalLM
+import mteb
+from sys import argv
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+model_name = 'neox_rpt_model'
+hf_model = GPTNeoXForCausalLM.from_pretrained(argv[1])
+hf_model.to(device=device)
+
+evaluation = mteb.MTEB(tasks=[Banking77Classification(hf_subsets=["en"], batch_size=16)])
+results = evaluation.run(hf_model, output_folder=f"results/{model_name}")
+
