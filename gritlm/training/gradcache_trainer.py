be94b9a2dbf163745b46cfcb5f742552cb2ae211
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-08-11 20:29:27 +0300

    .

--- gritlm/training/gradcache_trainer.py
+++ gritlm/training/gradcache_trainer.py
@@ -779,7 +779,7 @@ class GradCacheTrainer(Trainer):
                     self.state.epoch = epoch + (step + 1 + steps_skipped) / steps_in_epoch
                     self.control = self.callback_handler.on_step_end(args, self.state, self.control)
 
-                    self._maybe_log_save_evaluate(tr_loss, model, trial, epoch, ignore_keys_for_eval)
+                    self._maybe_log_save_evaluate(tr_loss, None, model, trial, epoch, ignore_keys_for_eval)
                 else:
                     self.control = self.callback_handler.on_substep_end(args, self.state, self.control)
 
