a4d6cd8288dc690b80a028733f5898cdcad651a4
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-08-16 19:35:35 +0300

    .

--- scripts/double_reformat_statedict.py
+++ scripts/double_reformat_statedict.py
@@ -3,16 +3,13 @@ import torch
 
 sd_path = sys.argv[1]
 
-layer_prefix = 'model.gpt_neox.layers.'
 model_prefix = 'model.'
 
 sd = torch.load(sd_path)
 
 new_sd = {}
 for k, v in sd.items():
-    if k.startswith(layer_prefix):
-        new_value = 'gpt_neox.layers.blocks.' + k[len(layer_prefix):]
-    elif k.startswith(model_prefix):
+    if k.startswith(model_prefix):
         new_value = k[len(model_prefix):]
     else:
         new_value = k
