783e2750363eff8254bc82a6e9a2728fd82e9df3
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-08-16 19:22:50 +0300

    .

--- scripts/double_reformat_statedict.py
+++ scripts/double_reformat_statedict.py
@@ -8,11 +8,6 @@ layer_prefix = 'model.gpt_neox.layers.'
 model_prefix = 'model.'
 
 sd = torch.load(sd_path)
-# Check if already reformatted by checking if first key has model. prefix
-if not list(sd.keys())[0].startswith(model_prefix):
-    print('SD seems already reformatted: ', sd.keys())
-    sys.exit(0)
-# Remove model i.e. model.h.1 -> h.1
 
 new_sd = {}
 for k, v in sd.items():
