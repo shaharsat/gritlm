f43d72e1734d74cf79ef9f47996c38ceb524e863
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-08-16 19:01:38 +0300

    .

--- scripts/double_reformat_statedict.py
+++ scripts/double_reformat_statedict.py
@@ -5,9 +5,9 @@ sd_path = sys.argv[1]
 
 sd = torch.load(sd_path)
 # Check if already reformatted by checking if first key has model. prefix
-if not list(sd.keys())[0].startswith('ox.'):
+if not list(sd.keys())[0].startswith('model.model.layers.'):
     print('SD seems already reformatted: ', sd.keys())
     sys.exit(0)
 # Remove model i.e. model.h.1 -> h.1
-sd = {k[len('ox.layers.'):] + 'layers.blocks.' if k.startswith('ox.') else k: v for k, v in sd.items()}
+sd = {'layers.blocks.' + k['model.model.layers.':] if k.startswith('model.model.layers.') else k: v for k, v in sd.items()}
 torch.save(sd, sd_path)
