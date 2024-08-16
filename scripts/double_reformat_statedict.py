a5e41e9075afcdbd59d00b98a9c10f7ab0363003
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-08-16 19:21:00 +0300

    .

--- scripts/double_reformat_statedict.py
+++ scripts/double_reformat_statedict.py
@@ -5,6 +5,7 @@ sd_path = sys.argv[1]
 
 prefix = 'model.gpt_neox.'
 layer_prefix = 'model.gpt_neox.layers.'
+model_prefix = 'model.'
 
 sd = torch.load(sd_path)
 # Check if already reformatted by checking if first key has model. prefix
@@ -19,6 +20,8 @@ for k, v in sd.items():
         new_value = 'layers.blocks.' + k[len(layer_prefix):]
     elif k.startswith(prefix):
         new_value = k[len(prefix):]
+    elif k.startswith(model_prefix):
+        new_value = k[len(model_prefix):]
     else:
         new_value = k
 
