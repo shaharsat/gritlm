762f99200fe0f4bc7aa462a430a8a3b9696eadfa
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-09-07 21:07:52 +0300

    .

--- gritlm/models/modeling_llama.py
+++ gritlm/models/modeling_llama.py
@@ -983,7 +983,11 @@ class LlamaModel(LlamaPreTrainedModel):
 
         print('######## 3')
 
-        print(attention_mask.shape, inputs_embeds.shape, cache_position, past_key_values is not None, output_attentions)
+        print(attention_mask.shape)
+        print(inputs_embeds.shape)
+        print(cache_position)
+        print(past_key_values is not None)
+        print(output_attentions)
 
         causal_mask = self._update_causal_mask(
             attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions
