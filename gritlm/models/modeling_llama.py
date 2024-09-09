f6216cf9f0e90a119d0fe01337204b42861a4872
Shahar Satamkar <shahar.satamkar@fiverr.com>
2024-09-09 04:59:30 +0300

    .

--- gritlm/models/modeling_llama.py
+++ gritlm/models/modeling_llama.py
@@ -977,6 +977,7 @@ class LlamaModel(LlamaPreTrainedModel):
 
         print(f'cache_position is None={cache_position is None}')
         print(f'past_key_values={past_key_values}')
+        print(f'attention_mask 2: {attention_mask}')
 
         if cache_position is None:
             past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
